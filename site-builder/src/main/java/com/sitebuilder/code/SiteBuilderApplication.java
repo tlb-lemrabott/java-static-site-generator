99328a5c252e9f9d0eb173d28fcd42fffcaa1543
tlb-lemrabott <lemrabott.toulba@yahoo.com>
2025-10-27 16:19:38 -0400

    Add site-builder project files

---
+++ site-builder/src/main/java/com/sitebuilder/code/SiteBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.sitebuilder.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteBuilderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiteBuilderApplication.class, args);
+    }
+
+}
